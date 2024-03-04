a46ac210d947413e4871fc2a2b8269a396d3e7e4
reviewkeli <reviewkeli@tencent.com>
2024-03-04 11:04:13 +0800

    feat(config): 安装lodash

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,20 @@
 import '@umijs/max/typings';
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.svg' {
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const url: string;
+  export default url;
+}
+
+// 声明define中注入的环境变量，全部用“REACT_APP_”作为前缀
+declare const REACT_APP_ENV: string;
+
+interface IRetList<T = any> {
+  list: T[] | null;
+  total: number;
+}
